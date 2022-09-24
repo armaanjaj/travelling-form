dae35f35c530d81c2bd9c81f4671bd0f4bf9c2c3
armaanjaj <armaan.jaj@gmail.com>
2022-09-23 21:15:55 -0600

    Add extra functionalities and form validation

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+    // declare global variables
     $insert = false;
-    if(isset($_POST['f_name'])){
+    $f_nameErr = $l_nameErr = $emailErr = $phoneErr = $genderErr = $ageErr = "";
+    $f_name = $l_name = $email = $phone = $gender = $age = "";
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
         // set connection variables
         $server = "localhost";
         $username = "root";
@@ -14,24 +17,71 @@
             die("Connection to this database failed due to" . mysqli_connect_error());
         }
 
-        // read the incoming parameters
-        $f_name = $_POST['f_name'];
-        $l_name = $_POST['l_name'];
-        $age = $_POST['age'];
-        $email = $_POST['email'];
-        $gender = $_POST['gender'];
-        $phone = $_POST['phone'];
-        $other = $_POST['desc'];
+        $error_exists = false;
 
-        // enter values in the queries
-        $sql_query = "INSERT INTO `travelling_buddy`.`trip` (`f_name`, `l_name`, `email`, `phone`, `age`, `gender`, `other`) VALUES('$f_name', '$l_name', '$email', '$phone', '$age', '$gender', '$other')";
-
-        // check if query runs successfully
-        if($con->query($sql_query) == true){
-            $insert = true;
+        function validate($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
         }
-        else{
-            echo "Error: $sql <br> $con->error";
+
+        // read the incoming parameters
+        if (empty($_POST["f_name"])) {
+            $f_nameErr = "First Name is required";
+            $error_exists = true;
+        } else {
+            $f_name = validate($_POST['f_name']);
+        }
+
+        if (empty($_POST["l_name"])) {
+            $l_nameErr = "Last name is required";
+            $error_exists = true;
+        } else {
+            $l_name = validate($_POST['l_name']);
+        }
+
+        if (empty($_POST["age"])) {
+            $ageErr = "Age is required";
+            $error_exists = true;
+        } else {
+            $age = validate($_POST['age']);
+        }
+
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is required";
+            $error_exists = true;
+        } else {
+            $email = validate($_POST['email']);
+        }
+
+        if (empty($_POST["gender"])) {
+            $genderErr = "Gender is required";
+            $error_exists = true;
+        } else {
+            $gender = validate($_POST['gender']);
+        }
+
+        if (empty($_POST["phone"])) {
+            $phoneErr = "Phone is required";
+            $error_exists = true;
+        } else {
+            $phone = validate($_POST['phone']);
+        }
+
+        $other = validate($_POST['desc']);
+
+        if(!$error_exists){
+            // enter values in the queries
+            $sql_query = "INSERT INTO `travelling_buddy`.`trip` (`f_name`, `l_name`, `email`, `phone`, `age`, `gender`, `other`) VALUES('$f_name', '$l_name', '$email', '$phone', '$age', '$gender', '$other')";
+
+            // check if query runs successfully
+            if($con->query($sql_query) == true){
+                $insert = true;
+            }
+            else{
+                echo "Error: $sql <br> $con->error";
+            }
         }
 
         // close the connection
@@ -56,27 +106,43 @@
 
         <?php
 
+        $action = htmlspecialchars($_SERVER['PHP_SELF']);
+        $file_name = substr($action, strrpos($action, '/', 0)+1);
+
         if($insert == true){
-            echo "<h4 class='confirmation'>Thanks for submitting the form. See you soon.</h4>";
+            echo "<p class='confirmation'>Thanks for submitting the form. See you soon.</p>";
         }
         else{
-            echo '<form action="index.php" method="POST" class="form">
+            echo "<form action=".$file_name." method='POST' class='form'>
                 <fieldset>
-                    <input type="text" name="f_name" placeholder="Enter your first name"><br>
-                    <input type="text" name="l_name" placeholder="Enter your last name"><br>
-                    <input type="email" name="email" placeholder="Enter your email"><br>
-                    <input type="tel" name="phone" placeholder="Enter your phone number" maxlength="10"><br>
-                    <input type="text" name="age" placeholder="Enter your age"><br>
-                    <input type="text" name="gender" placeholder="Enter your gender"><br>
-                    <textarea name="desc" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
-                    <br>
-                    <input type="submit" class="btn submit" value="Go">
-                    <input type="reset" class="btn reset">
+                    <input type='text' name='f_name' value='".$f_name."' placeholder='Enter your first name'>
+                    <span class='error'>".$f_nameErr."</span><br>
+
+                    <input type='text' name='l_name' value='".$l_name."' placeholder='Enter your last name'>
+                    <span class='error'>".$l_nameErr."</span><br>
+
+                    <input type='email' name='email' value='".$email."' placeholder='Enter your email'>
+                    <span class='error'>".$emailErr."</span><br>
+
+                    <input type='tel' name='phone' value='".$phone."' placeholder='Enter your phone number' maxlength='10'>
+                    <span class='error'>".$phoneErr."</span><br>
+
+                    <input type='text' name='age' value='".$age."' placeholder='Enter your age'>
+                    <span class='error'>".$ageErr."</span><br>
+
+                    <input type='text' name='gender' value='".$gender."' placeholder='Enter your gender'>
+                    <span class='error'>".$genderErr."</span><br>
+
+                    <textarea name='desc' cols='30' rows='10' placeholder='Enter any other information here'></textarea><br>
+
+                    <input type='submit' class='btn submit' value='Go'>
+                    <input type='reset' class='btn reset'>
                 </fieldset>
-            </form>';
+            </form>";
         }
 
         ?>
     </div>
 </body>
+    <?php //include 'footer.php';?>
 </html>
